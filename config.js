a10747f87bb67e303c9825d625860e623619a44f
CuervoOFC <Ismaelsolis151@gmail.com>
2026-01-11 20:18:02 -0600

    Update config.js

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 const config = {
-  nombrebot: 'Meow WaBot',
+  nombrebot: 'YAKUSA',
   moneda: '$ᴅᴏʟᴀʀᴇs',
-  apikey: '', // Pon tu apikey aqui, consiguela en: https://api-adonix.ultraplus.click
+  apikey: 'AdonixKey59mrf95244', // Pon tu apikey aqui, consiguela en: https://api-adonix.ultraplus.click
   prefijo: '.',
 
   owner: [
